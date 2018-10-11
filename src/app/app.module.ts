5e984945f55b97eff3a146794cfc15a7f2ece456
muhamma1 <amwork647@gmail.com>
2018-10-11 17:58:09 -0400

    NgxsRouterPluginModule added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsRouterPluginModule } from "@ngxs/router-plugin";
 import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +17,7 @@ import { states } from "./states";
     NgxsReduxDevtoolsPluginModule.forRoot({
       disabled: environment.production
     }),
+    NgxsRouterPluginModule.forRoot(),
     NgxsLoggerPluginModule.forRoot({ logger: console, collapsed: false }),
     NgxsModule.forRoot(states),
     AppRoutingModule
