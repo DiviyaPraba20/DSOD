7dc4303497cb255464a6464dbed8d466850ca095
muhamma1 <amwork647@gmail.com>
2018-10-16 13:08:36 -0400

    DSTP-41 - Added html for the component - Font faces

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { CarouselComponent} from './carousel.component'
+import { DSOFeaturedTopicsComponent } from './featured-topics.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent,CarouselComponent]
+  declarations: [HomeComponent, CarouselComponent, DSOFeaturedTopicsComponent]
 })
 export class HomeModule { }
