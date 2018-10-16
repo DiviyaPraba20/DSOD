65d6320378bbc4648b21ad3245f5dfa7aef7cd68
muhamma1 <amwork647@gmail.com>
2018-10-16 16:53:16 -0400

    added topic component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DSODRatingComponent } from './rating.component';
+import { DSODTopicComponent } from './topic.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [DSODRatingComponent],
-  exports: [DSODRatingComponent]
+  declarations: [DSODRatingComponent, DSODTopicComponent],
+  exports: [DSODRatingComponent, DSODTopicComponent]
 })
 export class SharedModule {}
---
+++ src/app/shared/topic.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: 'dsod-topic',
+  template: ` 
+         <div class="feautred-block feautred-block-sm">
+						<div class="feautred-block-header">
+								<h4>{{topic.title}}</h4>
+								<dsod-rating></dsod-rating>
+						</div>
+						<div class="feautred-block-thumbnail">
+								<img *ngIf="topic.image" src={{topic.image}} class="img-fluid" alt="Feautred Thumbnail Large">
+						</div>
+						<div class="feautred-block-content">
+								<h2 class="topic-title mb-1">{{topic.summary}} </h2>
+								<em>by DSODentist <a href="" class="read-about">
+										<img src={{topic.icon}} alt="">
+								</a> </em>
+								<p class="mt-3">{{topic.description}}<a href="#">[more]</a></p>
+						</div>
+					</div>`,
+  styleUrls: ['./topic.component.scss']
+})
+export class DSODTopicComponent {
+  @Input()
+  topic;
+
+  constructor() {}
+}
