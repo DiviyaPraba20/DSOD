1a99cc1dfcbe894af3a7e311d3cd1e26c3027c2a
muhamma1 <amwork647@gmail.com>
2018-10-18 12:35:03 -0400

    DSTP-49

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -6,11 +6,12 @@ import { LayoutComponent } from './layout.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarService } from './navbar/services';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { DSODFooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, BsDropdownModule.forRoot()],
-  declarations: [LayoutComponent, NavbarComponent],
-  exports: [NavbarComponent],
+  declarations: [LayoutComponent, NavbarComponent, DSODFooterComponent],
+  exports: [NavbarComponent, DSODFooterComponent],
   providers: [NavbarService]
 })
 export class LayoutModule {}
