3571392afd1801746e647bdf2b7274691c981110
muhamma1 <amwork647@gmail.com>
2018-10-22 09:52:09 -0400

    added event page left scetion template

--- src/app/pages/events/components/index.ts
+++ src/app/pages/events/components/index.ts
@@ -1,10 +1,13 @@
 import { DSODEventCardComponent } from './card/event-card.component';
 import { DSODEventSlideComponent } from './slide/event-slide.component';
+import { DSODEventTrainingComponent } from './training/training.component';
 
 export * from './card/event-card.component';
 export * from './slide/event-slide.component';
+export * from './training/training.component';
 
 export const EVENTS_COMPONENTS = [
   DSODEventCardComponent,
-  DSODEventSlideComponent
+  DSODEventSlideComponent,
+  DSODEventTrainingComponent
 ];
