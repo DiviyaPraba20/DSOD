99ce58be79011a1869d116249ead60b7a3a188fb
muhamma1 <amwork647@gmail.com>
2018-10-24 13:22:15 -0400

    added router links for pages and subcomponents for podcast playlist

--- src/app/pages/podcast/components/index.ts
+++ src/app/pages/podcast/components/index.ts
@@ -1,7 +1,9 @@
 import { DSODPodcastContentComponent } from './podcast-content/podcast-content.component';
 import { DSODPodcastTabsComponent } from './podcast-tabs/podcast.tabs.component';
+import { DSODPodcastPlayListComponent } from './play-list/play-list.component';
 
 export const PODCAST_COMPONENTS = [
   DSODPodcastContentComponent,
-  DSODPodcastTabsComponent
+  DSODPodcastTabsComponent,
+  DSODPodcastPlayListComponent
 ];
---
+++ src/app/pages/podcast/components/play-list/play-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dsod-podcast-playlist',
+  templateUrl: './play-list.component.html',
+  styleUrls: ['./play-list.component.scss']
+})
+export class DSODPodcastPlayListComponent {}
