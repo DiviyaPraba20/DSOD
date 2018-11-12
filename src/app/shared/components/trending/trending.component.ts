ea962170e25deee1cacc5f0c83f14e99359e67cd
muhamma1 <amwork647@gmail.com>
2018-11-12 11:32:05 -0500

    updated shared components styles

--- src/app/shared/components/trending/trending.component.ts
+++ src/app/shared/components/trending/trending.component.ts
@@ -1,29 +1,41 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CMSPageContent } from 'src/app/cms/models';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'dsod-trending-item',
-  template: `<a *ngIf="trendingItem.videos" [routerLink]="['/video']"><div class="trending-block">
-							<div class="trending-block-header">
-								<h4>{{trendingItem.categoryName}}</h4>
-								<dsod-rating [rating]="trendingItem.avgCommentRating"></dsod-rating>
-							</div>
-							<div class="trending-block-video">
-								<video  src="https://devcmsapi1.dsodentist.com/content/contentservice/v1/file/downloadFileByObjectId?objectId={{trendingItem.videos[0]}}" controls="false" poster="https://devcmsapi1.dsodentist.com/content/contentservice/v1/file/downloadFileByObjectId?objectId={{trendingItem.featuredMediaId}}"></video>
-							</div>
-							<div class="trending-block-caption">
-								<h4>{{trendingItem.title}}</h4>
-							</div>
-					</div></a>`,
+  template: `
+    <a [routerLink]="['/video', trendingItem.id]"
+      ><div class="trending-block">
+        <div class="trending-block-header">
+          <h4>{{ trendingItem.categoryName }}</h4>
+          <dsod-rating [rating]="trendingItem.avgCommentRating"></dsod-rating>
+        </div>
+        <div class="trending-block-video" [innerHTML]="iframe"></div>
+        <div class="trending-block-caption">
+          <h4>{{ trendingItem.title }}</h4>
+        </div>
+      </div></a
+    >
+  `,
   styleUrls: ['./trending.component.scss']
 })
-export class DSODTrendingItemComponent {
+export class DSODTrendingItemComponent implements OnInit {
   @Input()
   trendingItem: CMSPageContent;
   @Input()
   showCaption: boolean;
   @Input()
   showSummary: boolean;
+  iframe: any;
 
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    const regex = /(<iframe.+?<\/iframe>)/g;
+    this.iframe = this.trendingItem.content.match(regex).map(v => {
+      return v;
+    });
+    this.iframe = this.sanitizer.bypassSecurityTrustHtml(this.iframe[0]);
+  }
 }
