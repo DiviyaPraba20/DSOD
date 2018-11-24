efd3dd320c6b17e00a844e01598f4ec62e040850
Ali <a.muhamma1@gmail.com>
2018-11-24 15:00:47 -0500

    used ngx package for device info

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,23 +2,21 @@ import { Injectable, Inject } from '@angular/core';
 
 import { DOCUMENT } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Injectable({ providedIn: 'root' })
 export class ApplicationStateService {
-    private isMobileResolution: boolean;
+  private isMobileResolution: boolean;
 
-    constructor(@Inject(DOCUMENT) private document: Document) {
-        if (environment.production) {
-            if (window.innerWidth < 768) {
-                this.isMobileResolution = true;
-                this.document.location.href = 'https://mobile.dsodentist.com';
-            } else {
-                this.isMobileResolution = false;
-            }
-        }
+  constructor(@Inject(DOCUMENT) private document: Document, private deviceService: DeviceDetectorService) {
+    if (environment.production) {
+      if (this.deviceService.isMobile() || this.deviceService.isTablet()) {
+        this.document.location.href = 'https://mobile.dsodentist.com';
+      }
     }
+  }
 
-    public getIsMobileResolution(): boolean {
-        return this.isMobileResolution;
-    }
+  public getIsMobileResolution(): boolean {
+    return this.deviceService.isMobile();
+  }
 }
