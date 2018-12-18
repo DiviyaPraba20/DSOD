c660ab988e977a26b974773b69cce0f502a6c4f1
BearandYoon <luckysevenbear@gmail.com>
2018-12-18 09:21:52 +0100

    implemented categoryPage

---
+++ src/app/pages/education/components/education-sub-category/education-sub-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dsod-education-sub-category',
+  templateUrl: './education-sub-category.component.html',
+  styleUrls: ['./education-sub-category.component.scss']
+})
+export class EducationSubCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/education/education.module.ts
+++ src/app/pages/education/education.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 import { EducationRoutingModule } from './education-routing.module';
-import { EducationComponent } from './components/education/education.component';
+import { EducationContainerComponent } from './container/education-container.component';
+import { EducationCatalogueComponent } from './components/education-catalogue/education-catalogue.component';
+import { EducationSubCategoryComponent } from './components/education-sub-category/education-sub-category.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    EducationRoutingModule
+    EducationRoutingModule,
+    TabsModule.forRoot()
   ],
-  declarations: [EducationComponent]
+  declarations: [
+    EducationContainerComponent,
+    EducationCatalogueComponent,
+    EducationSubCategoryComponent
+  ]
 })
 export class EducationModule { }
