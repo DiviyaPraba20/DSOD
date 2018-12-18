e31f594f6251f3f9ec49cdad71633cc2da664702
muhamma1 <amwork647@gmail.com>
2018-12-18 16:44:45 -0500

    DSTP-304

--- src/app/pages/video/components/video-content/video-content.component.ts
+++ src/app/pages/video/components/video-content/video-content.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CMSPageContent } from 'src/app/cms/models';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'dsod-video-content',
   templateUrl: './video-content.component.html',
   styleUrls: ['./video-content.component.scss']
 })
-export class DSODVideoContentComponent {
+export class DSODVideoContentComponent implements OnInit {
   @Input()
   content: string;
-  constructor() {}
+  contentString:any
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.contentString = this.sanitizer.bypassSecurityTrustHtml(this.content);
+  }
 }
