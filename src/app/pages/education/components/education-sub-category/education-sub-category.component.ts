59967a2a090f9a02c69e93335dbfb0f75ec06f3e
BearandYoon <luckysevenbear@gmail.com>
2018-12-19 18:41:29 +0100

    fixed DSTP-286

--- src/app/pages/education/components/education-sub-category/education-sub-category.component.ts
+++ src/app/pages/education/components/education-sub-category/education-sub-category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DSODEducationCategoryData } from '../education-catalogue/education-catalogue.component';
+import { DSOEducationCategory } from '../../models/category.model';
 
 @Component({
   selector: 'dsod-education-sub-category',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education-sub-category.component.scss']
 })
 export class EducationSubCategoryComponent implements OnInit {
+  categoryType = '';
+  categoryInfo: DSOEducationCategory;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.categoryType = params.categoryName;
+      this.getCategoryName();
+    });
   }
 
+  getCategoryName() {
+    this.categoryInfo = DSODEducationCategoryData.filter(data => data.name === this.categoryType)[0];
+  }
 }
