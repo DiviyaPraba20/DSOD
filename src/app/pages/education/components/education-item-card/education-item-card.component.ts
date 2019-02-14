ef3c2716c5927433a69f133ad5598f695e11379e
muhamma1 <amwork647@gmail.com>
2019-02-14 12:16:49 -0500

    added loader image

--- src/app/pages/education/components/education-item-card/education-item-card.component.ts
+++ src/app/pages/education/components/education-item-card/education-item-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input } from '@angular/core';
+import { debug } from 'util';
 
 @Component({
   selector: 'dsod-education-item-card',
@@ -7,6 +8,7 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input } from '
 })
 export class EducationItemCardComponent implements OnInit, AfterViewInit {
   video:any;
+  bufferdLength:any;
   @ViewChild('video') myDiv: ElementRef;
   @Input() sampleVideo:any;
   constructor() {}
@@ -14,28 +16,24 @@ export class EducationItemCardComponent implements OnInit, AfterViewInit {
   ngOnInit() {}
 
   ngAfterViewInit(){
-    this.video = this.myDiv.nativeElement;
+    this.video = this.myDiv.nativeElement
     this.video.removeAttribute('controls');
     this.video.addEventListener('ended', ()=> {
       this.video.play();
     });
+
+    this.video.addEventListener('progress', (v) => {
+      this.video.removeAttribute('poster');
+    });
   }
 
   onMouseOver(e) {
-    let playPromise = this.video.play();
-
-    if (playPromise !== undefined) {
-      playPromise.then(_ => {
-        this.video.play()
-      })
-        .catch(error => {
-        });
-    }
-    
+      this.video.play();
   }
 
   onMouseLeave(e) {
     this.video.currentTime=0
     this.video.pause();
   }
+
 }
