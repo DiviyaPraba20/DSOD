3dcecc8366791eefbadca2724b5571c782a4a403
BearandYoon <luckysevenbear@gmail.com>
2019-02-21 19:46:06 +0100

    - implemented navMenu for podcast category

--- src/app/pages/dso-practice/containers/dso-practice/dso-practice.component.ts
+++ src/app/pages/dso-practice/containers/dso-practice/dso-practice.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { CMSContentParams, CMSContentTypeModel, CMSPageContent } from 'src/app/cms/models';
 import { Store, Actions, ofActionDispatched } from '@ngxs/store';
-import { FetchContentTypes, FetchSponsorsList, ResetState, FetchSearchResultsSuccess, FetchSponsoredTopics, FetchDSOPracticesSuccess, FetchDSOPractices, FetchSponsorContentsSuccess, FetchCategories } from 'src/app/cms/actions';
+import { FetchContentTypes, ResetState, FetchSponsoredTopics, FetchDSOPracticesSuccess, FetchDSOPractices, FetchSponsorContentsSuccess, FetchCategories } from 'src/app/cms/actions';
 import { skip } from 'rxjs/operators';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ActivatedRoute } from '@angular/router';
@@ -25,18 +25,24 @@ export class DSODPracticePageComponent implements OnInit, OnDestroy {
   other = [];
   selectedTab = 'All';
   contentTypeId: string;
-  categoryId:string;
+  categoryId: string;
   contentTypes: CMSContentTypeModel[];
-  isLoading:boolean=true;
-  title:string;
-  constructor(private store: Store, private actions$: Actions, private spinnerService: NgxSpinnerService, private route:ActivatedRoute) {
+  isLoading = true;
+  title: string;
+
+  constructor(
+    private store: Store,
+    private actions$: Actions,
+    private spinnerService: NgxSpinnerService,
+    private route: ActivatedRoute
+  ) {
     store.dispatch(new FetchContentTypes());
     store.dispatch(new FetchCategories());
-    this.route.params.pipe().subscribe(param=>{
-      this.categoryId = param.id
-    })
-    
+    this.route.params.pipe().subscribe(param => {
+      this.categoryId = param.id;
+    });
   }
+
   ngOnInit() {
     this.contentTypes$ = this.store.select(state => state.cms.contentTypes);
     this.sponsorsList$ = this.store.select(state => state.cms.sponsorsList);
@@ -58,26 +64,26 @@ export class DSODPracticePageComponent implements OnInit, OnDestroy {
       )
       .subscribe(data => {
         this.spinnerService.show();
-        this.isLoading=true;
-      });  
+        this.isLoading = true;
+      });
     this.actions$
       .pipe(ofActionDispatched(FetchSponsorContentsSuccess, FetchDSOPracticesSuccess))
       .subscribe(data => {
         this.spinnerService.hide();
-        this.isLoading=false;
+        this.isLoading = false;
       });
 
-    this.categoriesSub= this.store.select(state => state.cms.categories).pipe(skip(1)).subscribe(data=>{
-      let categories = data.filter(item => item.id == this.categoryId)
-      this.title=categories[0].name
-    })
+    this.categoriesSub = this.store.select(state => state.cms.categories).pipe(skip(1)).subscribe(data => {
+      const categories = data.filter(item => item.id === this.categoryId);
+      this.title = categories[0].name;
+    });
   }
 
   filterType(tabName: string) {
-    if (tabName == 'All') {
+    if (tabName === 'All') {
       this.contentTypeId = null;
     } else {
-      const type = this.contentTypes.filter(item => item.name == tabName);
+      const type = this.contentTypes.filter(item => item.name === tabName);
       this.contentTypeId = type[0].id;
     }
     this.selectedTab = tabName;
--- src/app/pages/home/components/podcasts/podcasts.component.ts
+++ src/app/pages/home/components/podcasts/podcasts.component.ts
@@ -1,12 +1,9 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import {
-  CMSPageContent,
-  CMSContentParams,
-  CMSContentTypeModel
-} from 'src/app/cms/models';
+import { Component, Input, OnInit } from '@angular/core';
+import { CMSPageContent, CMSContentParams, CMSContentTypeModel } from 'src/app/cms/models';
 import { Store } from '@ngxs/store';
 import * as actions from '../../../../cms/actions';
 import { Observable } from 'rxjs';
+import { CMSService } from '../../../../cms/services/cms.service';
 
 @Component({
   selector: 'dsod-podcasts',
@@ -14,8 +11,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./podcasts.component.scss']
 })
 export class DSODPodcastsComponent implements OnInit {
-  @Input()
-  contentType: CMSContentTypeModel;
+  @Input() contentType: CMSContentTypeModel;
 
   podcasts$: Observable<CMSPageContent[]>;
   indexStart = 0;
@@ -27,7 +23,13 @@ export class DSODPodcastsComponent implements OnInit {
   };
   podcastItem: number;
   imageIndex = 0;
-  constructor(private store: Store) {}
+  categories: CMSContentTypeModel[] = [];
+  selectedType = 'new';
+
+  constructor(
+    private store: Store,
+    private cmsService: CMSService
+  ) {}
 
   ngOnInit() {
     this.store.dispatch(
@@ -37,11 +39,18 @@ export class DSODPodcastsComponent implements OnInit {
       })
     );
     this.podcasts$ = this.store.select(state => state.cms.podcasts);
+
+    this.cmsService.findAllCategory().subscribe(res => {
+      if (res.code === 0) {
+        console.log(res.resultMap.data);
+        this.categories = res.resultMap.data;
+      }
+    });
   }
 
   podcastPagination(e?, page?) {
     this.activePage = page;
-    if (page == 1) {
+    if (page === 1) {
       this.indexStart = 0;
       this.indexEnd = 9;
       this.imageIndex = 0;
@@ -59,5 +68,15 @@ export class DSODPodcastsComponent implements OnInit {
       this.activePage -= 1;
     }
     this.podcastPagination(null, this.activePage);
+  }
+
+  selectNewType() {
+    console.log('selectNewType');
+    this.selectedType = 'new';
+  }
+
+  changeCategory(category: CMSContentTypeModel) {
+    console.log(category);
+    this.selectedType = 'category';
   }
 }
