d8bb825dccc7453978cadd59007d9762f353a787
muhamma1 <amwork647@gmail.com>
2019-03-01 22:45:57 -0500

    minor fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -72,7 +72,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', onSameUrlNavigation: "reload" })
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled'})
   ],
   exports: [RouterModule]
 })
